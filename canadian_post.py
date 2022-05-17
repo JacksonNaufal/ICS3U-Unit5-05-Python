8b6b867ef0d5be9464ccd2ff7002ded9e70dedab
Jackson Naufal <Jackson.naufal@mths.ca>
2022-05-17 18:19:39 +0000

    FinalFixedErrors

--- canadian_post.py
+++ canadian_post.py
@@ -2,89 +2,74 @@
 
 # Created by: Jackson Naufal
 # Created on: May 2022
-# This is a address post code 
+# This is a address post code
 
 
-
-def post_function(fullname, 
-    streetnum, 
-    streetname,
-    city,
-    province, 
-    postalcode, 
-    apartment = None):
+def post_function(
+    full_name, street_num, street_name, city, province, postal_code, apartment=None
+):
 
     # output
-    
-    if apartment is not None:
-        post_code = "{0} \n{1}-{2} {3} \n{4} {5} {6}".format(
-        fullname, 
-        apartment, 
-        streetnum, 
-        streetname, 
-        city, 
-        province, 
-        postalcode)
-    
+    if apartment != None:
+        post_code = "\n{0} \n{1}-{2} {3} \n{4} {5} {6}".format(
+            full_name, street_num, street_name, city, province, postal_code, apartment
+        )
+
     else:
-        post_code = "{0} \n{1} {2} \n{3} {4} {5}".format(fullname, 
-        streetnum, 
-        streetname, 
-        city, 
-        province, 
-        postalcode)
-    
+        post_code = "\n{0} \n{1} {2} \n{3} {4} {5}".format(
+            full_name, street_num, street_name, city, province, postal_code
+        )
+
     return post_code.upper()
-    
 
 
 def main():
 
     apartment = None
-    
+
     # input
-    fullname = input("Enter your fullname! (first, middle, last): ")
+    full_name = input("Enter your full_name! (first, middle, last): ")
     question = input("Do you live in an apartment(y/n)?): ")
     if question.upper() == "Y" or question.upper() == "YES":
         apartment = input("Enter your apartment number!: ")
-    streetnum = input("Enter your street number!: ")
-    streetname = input("Enter street name AND type!: ")
+    street_num = input("Enter your street number!: ")
+    street_name = input("Enter street name AND type!: ")
     city = input("Enter your city!: ")
     province = input("Enter your province!: ")
-    postalcode = input("Enter your postal code (LNL NLN): ")
-                        
+    postal_code = input("Enter your postal code (LNL NLN): ")
 
     # process
     try:
-        street_number_int = int(streetnum)
-        
+        street_number_int = int(street_num)
+
         if street_number_int < 0:
             print("\nInvalid Street Number")
         elif apartment is not None:
             apartment_number_int = int(apartment)
             if apartment_number_int < 0:
-              print("\nInvalid Street Number")
-            else: 
+                print("\nInvalid Street Number")
+            # call functions
+            else:
                 information = post_function(
-                    fullname, 
-                    apartment, 
-                    streetnum, 
-                    streetname, 
-                    city, 
-                    province, 
-                    postalcode, 
-                    )
-                print (information)
+                    full_name,
+                    apartment,
+                    street_num,
+                    street_name,
+                    city,
+                    province,
+                    postal_code,
+                )
+                print(information)
         else:
             information = post_function(
-                fullname, 
-                streetnum, 
-                streetname, 
-                city, 
-                province, 
-                postalcode, 
-                )
-            print (information)
+                full_name,
+                street_num,
+                street_name,
+                city,
+                province,
+                postal_code,
+            )
+            print(information)
 
     except Exception:
         print("Not An Integer")
